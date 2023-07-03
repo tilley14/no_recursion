4bb8a6237cd3dce9a5f039823ec825bce5366904
Windows NT <nmt2400@g.rit.edu>
2023-07-03 11:41:45 -0400

    Create merge_sort.cpp

---
+++ sort/merge_sort.cpp
@@ -0,0 +1,12 @@
+#include <stdint>
+
+// TODO: templatize
+void merge_sort(std::vector<int> &in)
+{
+    // 0 or 1 elements is already sorted
+    if (in.size() < 2)
+        return;
+
+    // divide and conquor 
+    
+}
